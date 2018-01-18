b39e72b1f01a9548d359c68c356ca6f23eb953f0
wuchaoml <wuchaoml@gmail.com>
2018-01-18 11:14:21 +0800

    0.944

--- config.py
+++ config.py
@@ -12,21 +12,21 @@ class TrainingConfig(object):
 class ModelConfig(object):
     conv_layers = [[256, 5, 3],
                    [256, 5, 3],
-                   [256, 2, None],
-                   [256, 2, None],
-                   [256, 2, None],
-                   [256, 2, 3]]
+                   [256, 3, None],
+                   [256, 3, None],
+                   [256, 3, None],
+                   [256, 3, 3]]
 
-    fully_connected_layers = [256, 128, 64]
+    fully_connected_layers = [512, 256, 128]
     th = 1e-6
 
 
 class Config(object):
-    max_length = 128
-    batch_size = 512
+    max_length = 200
+    batch_size = 64
     no_of_classes = 2
     learning_rate = 0.00001
-    beta = 7
+    beta = 0.035
 
     training = TrainingConfig()
     model = ModelConfig()
@@ -34,7 +34,7 @@ class Config(object):
 
 config = Config()
 
-'''0.924
+'''0.9436 35.76k (1,2) 1516237999
 class TrainingConfig(object):
     p = 0.5
     base_rate = 1e-2
@@ -47,29 +47,30 @@ class TrainingConfig(object):
 
 
 class ModelConfig(object):
-    conv_layers = [[128, 5, 3],
-                   [128, 5, 3],
-                   [128, 2, None],
-                   [128, 2, None],
-                   [128, 2, None],
-                   [128, 2, 3]]
+    conv_layers = [[256, 5, 3],
+                   [256, 5, 3],
+                   [256, 3, None],
+                   [256, 3, None],
+                   [256, 3, None],
+                   [256, 3, 3]]
 
-    fully_connected_layers = [256, 128, 64]
+    fully_connected_layers = [512, 256, 128]
     th = 1e-6
 
 
 class Config(object):
-    max_length = 128
-    batch_size = 512
+    max_length = 200
+    batch_size = 64
     no_of_classes = 2
     learning_rate = 0.00001
-    beta = 3.5
+    beta = 0.035
 
     training = TrainingConfig()
     model = ModelConfig()
 
 
 config = Config()
+
 '''
 
 '''0.9424 17k
@@ -112,6 +113,7 @@ config = Config()
 '''
 
 
+
 '''0.948,step = 11k
 class TrainingConfig(object):
     p = 0.5
@@ -148,44 +150,4 @@ class Config(object):
 
 
 config = Config()
-'''
-
-
-'''
-class TrainingConfig(object):
-    p = 0.5
-    base_rate = 1e-2
-    momentum = 0.9
-    decay_step = 15000
-    decay_rate = 0.95
-    epoches = 1000
-    evaluate_every = 10
-    checkpoint_every = 100
-
-
-class ModelConfig(object):
-    conv_layers = [[256, 5, 3],
-                   [256, 5, 3],
-                   [256, 2, None],
-                   [256, 2, None],
-                   [256, 2, None],
-                   [256, 2, 3]]
-
-    fully_connected_layers = [512, 256]
-    th = 1e-6
-
-
-class Config(object):
-    max_length = 200
-    batch_size = 256
-    no_of_classes = 2
-    learning_rate = 0.000025
-    beta = 0.01
-
-    training = TrainingConfig()
-    model = ModelConfig()
-
-
-config = Config()
-
 '''
